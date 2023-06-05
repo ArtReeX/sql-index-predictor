4d0a7a08393e101741de063bf2db92c0fd5717fe
Arthur Lazarenko <Arthur.Lazarenko@icloud.com>
2023-06-05 03:46:52 +0300

    init project

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,41 @@
 import { filter, isArray, isEmpty, isEqual, isString, reject, uniq, uniqWith } from "lodash";
-import { Expr, From, nil, parse } from "pgsql-ast-parser";
+import { Expr, SelectFromStatement, parse } from "pgsql-ast-parser";
 
 export class SqlIndexPredictor {
-  private handleGraph(data: Expr, from: From[] | nil): any[] {
-    if (data.type === "select" && data.where) {
-      return this.handleGraph(data.where, data.from);
+  private handleGraph(children: Expr, parent: SelectFromStatement): any[] {
+    if (children.type === "select" && children.where) {
+      return this.handleGraph(children.where, children);
     }
-    if (data.type === "unary") {
-      return this.handleGraph(data.operand, from);
+    if (children.type === "unary") {
+      return this.handleGraph(children.operand, parent);
     }
-    if (data.type === "call") {
-      return data.args.map((ast) => this.handleGraph(ast, ast.type === "select" ? ast.from : from));
+    if (children.type === "call") {
+      return children.args.map((ast) => this.handleGraph(ast, ast.type === "select" ? ast : parent));
     }
-    if (data.type === "binary") {
+    if (children.type === "binary") {
       // "OR" BINARY OPERATORS
-      if (data.op === "OR") {
-        return [this.handleGraph(data.left, from), this.handleGraph(data.right, from)];
+      if (children.op === "OR") {
+        return [this.handleGraph(children.left, parent), this.handleGraph(children.right, parent)];
       }
 
       // "AND" AND OTHERS BINARY OPERATORS
-      return [...this.handleGraph(data.left, from), ...this.handleGraph(data.right, from)];
+      return [...this.handleGraph(children.left, parent), ...this.handleGraph(children.right, parent)];
     }
-    if (data.type === "ref") {
-      for (const table of from || []) {
+    if (children.type === "ref") {
+      for (const table of parent.from || []) {
         // FIND A TABLE WITH AN ALIAS THE SAME AS IN THE QUERY
-        if (table.type === "table" && table.name.alias && data.table && table.name.alias === data.table.name) {
-          return [`${table.name.name}.${data.name}`];
+        if (table.type === "table" && table.name.alias && children.table && table.name.alias === children.table.name) {
+          return [`${table.name.name}.${children.name}`];
         }
       }
-      for (const table of from || []) {
+      for (const table of parent.from || []) {
         // FIND A TABLE WITH THE SAME NAME AS IN THE QUERY
-        if (table.type === "table" && !table.name.alias && data.table && table.name.name === data.table.name) {
-          return [`${table.name.name}.${data.name}`];
+        if (table.type === "table" && !table.name.alias && children.table && table.name.name === children.table.name) {
+          return [`${table.name.name}.${children.name}`];
         }
         // IF THE QUERY DOES NOT SPECIFY A TABLE, USE THE FIRST TABLE FROM THE LIST WITHOUT AN ALIAS
-        if (table.type === "table" && !table.name.alias && !data.table) {
-          return [`${table.name.name}.${data.name}`];
+        if (table.type === "table" && !table.name.alias && !children.table) {
+          return [`${table.name.name}.${children.name}`];
         }
       }
     }
@@ -54,7 +54,7 @@ export class SqlIndexPredictor {
   public predict(sqlQuery: string): string[][] {
     const predictedIndexes = parse(sqlQuery).reduce<string[][]>((indexes, ast) => {
       if (ast.type === "select") {
-        return [...indexes, ...this.handleGraph(ast, ast.from)];
+        return [...indexes, ...this.handleGraph(ast, ast)];
       }
 
       return [];
